0485224494bba8419ea5bed379f56643eae55e77
LeiMai <mike6453andy3181@gmail.com>
2025-07-02 20:44:53 +0800

    部署 Render 前準備

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-# config.py
-BOT_TOKEN = "7437904906:AAEXvRUxqPzOpa9ug9i-WDHFOSXbEVovVxw"
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1w9BZnMneByzIv3SDErdSKhXyfnNg5WSzN01Glk6-zo4/edit?gid=0#gid=0"
-GS_CREDENTIALS_FILE = "leimai-v0-6c91b6b47766.json"
-# 之後可加 GPT API KEY
+import os
+from dotenv import load_dotenv
+
+# 讀取本地 .env
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GS_CREDENTIALS_FILE = os.getenv("GS_CREDENTIALS_FILE")
+SHEET_URL = os.getenv("SHEET_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
